38412d5fa915ac14dc363457be27f7db85842241
Thomas <thomaskao99@gmail.com>
2015-07-22 13:59:14 +0800

    我的作業都寫完了

---
+++ HW2高培恩.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+
+var a:Double = 103.0
+var b:Double = 20.0
+var c:Double = a%b
+var d:Double = b/c
+print(c+d)
