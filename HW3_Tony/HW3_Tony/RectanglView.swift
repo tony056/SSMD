3a5b7ec0f45d0042923c63ea1726909b5786fa16
tony056 <tony61507@gmail.com>
2015-07-27 14:00:21 +0800

    my lecture slide

---
+++ HW3_Tony/HW3_Tony/RectanglView.swift
@@ -0,0 +1,9 @@
+//
+//  RectanglView.swift
+//  HW3_Tony
+//
+//  Created by TUNGYING-CHAO on 7/27/15.
+//  Copyright © 2015 TUNGYING-CHAO. All rights reserved.
+//
+
+import Foundation
---
+++ HW3_Tony/HW3_Tony/RectangleView.swift
@@ -0,0 +1,21 @@
+//
+//  RectangleView.swift
+//  HW3_Tony
+//
+//  Created by TUNGYING-CHAO on 7/27/15.
+//  Copyright © 2015 TUNGYING-CHAO. All rights reserved.
+//
+
+import UIKit
+
+class RectangleView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
